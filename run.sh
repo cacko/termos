76790423fb7a571ed20dfdaef3db775804c89226
cacko <alex@cacko.net>
2024-10-23 08:35:17 +0100

    I don't know what these changes are supposed to accomplish but somebody told me to make them.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 source ~/.zshrc
-direnv exec . mamba run --live-stream -n termo-service uvicorn 'termo_service.app:create_app' --host 0.0.0.0 --port 23727 --workers 1  --log-level info --use-colors --factory --loop uvloop --access-log
+direnv exec . sudo -E mamba run --live-stream -n termo-service uvicorn 'termo_service.app:create_app' --host 0.0.0.0 --port 23727 --workers 1  --log-level info --use-colors --factory --loop uvloop --access-log
