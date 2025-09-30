a8fdba68bfc06fb04ce439fca3354cad33e37552
cacko <alex@cacko.net>
2025-09-30 01:06:32 +0100

    "add taskfile.yml diff for 1.8.3.0-rc.1.2.2 and 1.9.3-rc-1.1-rc2.1_rc2_1_2_3."

--- src/termos/__main__.py
+++ src/termos/__main__.py
@@ -1,9 +1,5 @@
 import sys
-from termos.cli import main as cli
 from termos import start
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        sys.exit(cli())
-    else:
-        start()
+    sys.exit(start())
