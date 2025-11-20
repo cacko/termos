1bf9762aaaba13c8dbf948776dd41e5a13feb88a
cacko <alex@cacko.net>
2025-11-20 05:02:48 +0200

    assorted changes

--- src/termos/sensor/http.py
+++ src/termos/sensor/http.py
@@ -4,9 +4,11 @@ from asyncio import Queue
 from termos.sensor.sensor import Sensor
 from termos.sensor.models import NowData
 import httpx
+from httpx import ConnectTimeout
 from httpx_retries import RetryTransport, Retry
 
 from pydantic import BaseModel, AwareDatetime
+
 
 class NowResponse(BaseModel):
     temp: float
@@ -17,11 +19,11 @@ class HTTP(Sensor):
 
     ble_queue: Queue = Queue()
     datapoints_count = 0
-    
+
     @property
     def is_connected(self) -> bool:
         return True
-    
+
     def disconnect(self):
         pass
 
@@ -31,9 +33,9 @@ class HTTP(Sensor):
         retry = Retry(total=3, backoff_factor=0.5)
 
         async with httpx.AsyncClient(
-                base_url=f"http://{self.address}:{self.mac}",
-                transport=RetryTransport(retry=retry),
-            ) as client:
+            base_url=f"http://{self.address}:{self.mac}",
+            transport=RetryTransport(retry=retry),
+        ) as client:
             while client:
                 try:
                     r = await client.get(self.uuid_read)
@@ -44,8 +46,14 @@ class HTTP(Sensor):
                         humid=0,
                         location=self.location,
                     )
-                    logging.info(f"New data: {nowdata.temp}°C, {nowdata.humid}%, {self.__class__}")
+                    logging.info(
+                        f"New data: {nowdata.temp}°C, {nowdata.humid}%, {self.__class__}"
+                    )
                     self.__class__.queue.put_nowait((nowdata, self.__class__))
                     await asyncio.sleep(70)
+                except ConnectTimeout:
+                    logging.warning(
+                        f"cannot connect to http://{self.address}:{self.mac}/{self.uuid_read}"
+                    )
                 except Exception as e:
                     logging.exception(e)
